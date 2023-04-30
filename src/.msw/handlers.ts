104eb6294a6e1d7cb880c00a5cb0e38d420a1d86
yoonsang lee <highp0912@pm.me>
2023-05-01 03:09:41 +0900

    setup: msw

---
+++ src/.msw/handlers.ts
@@ -0,0 +1,23 @@
+import { rest } from "msw";
+
+const serverCfg = {
+  url: "http://localhost:3000"
+};
+
+interface Scoop {
+  name: Capitalize<"Chocolate" | "Vanilla">;
+  imagePath: string;
+}
+
+export const handler = [
+  rest.get<Scoop>(`${serverCfg.url}/scoops`, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        { name: "Chocolate", imagePath: "/images/chocolate.png" },
+        { name: "Vanilla", imagePath: "/images/vanilla.png" },
+        { name: "Strawberry", imagePath: "/images/strawberry.png" }
+      ])
+    );
+  })
+];
